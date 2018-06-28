3456609adabe6a1ea3a6a5fc3da34229e6809724
myrrkel <mp05.octave@octave.local>
2018-06-28 17:58:39 +0200

    radio manager

--- radioManager.py
+++ radioManager.py
@@ -69,14 +69,17 @@ class radioManager():
 
 
     def searchDarRadios(self,search):
-        darRadios = None
+        darRadios = []
+
         idList = self.searchDarRadiosID(search)
 
         for id in idList:
-            darStream = self.getDarStream(id)
-            print(darStream)
-            stream = self.getRedirection(darStream)
-            print(stream)
+            print("IDDar="+str(id))
+            darStation = radio()
+            darStation.stream, darStation.name = self.getDarStation(id)
+            darStation.stream = self.getRedirection(darStation.stream)
+            print(darStation.stream)
+            darRadios.append(darStation)
 
         return darRadios                 
 
@@ -100,11 +103,13 @@ class radioManager():
         return radIDList        
 
 
-    def getDarStream(self,id):
-        stream = ""
+    def getDarStation(self,id):
+        radio_url = ""
+        name = ""
 
         try:
             url = "http://www.dar.fm/uberstationurlxml.php?station_id="+str(id)+"&partner_token="+darAPIKey
+            print(url)
             r = requests.get(url)
             print(r.text)
             tree = ET.fromstring(r.text)
@@ -112,35 +117,37 @@ class radioManager():
         except requests.exceptions.HTTPError as err:  
             print(err)
 
-        for child in tree:
-            if child.tag == "url":
-                stream = child.text.strip()
-                print(stream)
-
-        return stream            
+        if tree.find("url") is not None:
+            radio_url = tree.find("url").text.strip()
+            name = tree.find("callsign").text.strip()
+         
+        return (radio_url,name)         
 
 
     def searchDirbleRadios(self,search):
-        dirbleRadios = None
+        dirbleRadios = []
         search = urllib.parse.quote_plus(search)
 
         try:
             r = requests.post("http://api.dirble.com/v2/search?token="+dirbleAPIKey, data={'query': search, 'page': 1})
-            dirbleRadios = json2obj(r.text)
+            dradios = json2obj(r.text)
         except requests.exceptions.HTTPError as err:  
             print(err)
 
-        return dirbleRadios
-
-    def search(self,search):
-        resRadios = []
-        dradios = self.searchDarRadios(search)
         if dradios:
             for dr in dradios:
                 for strm in dr.streams:
                         rad = radio()
                         rad.initWithDirbleRadio(dr,strm.stream)
-                        resRadios.append(rad)
+                        dirbleRadios.append(rad)
+
+        return dirbleRadios
+
+    def search(self,search):
+        resRadios = []
+        resRadios = self.searchDarRadios(search)
+        resRadios.extend(self.searchDirbleRadios(search))
+        
 
         return resRadios
 
@@ -154,4 +161,4 @@ if __name__ == "__main__":
     for rad in radios:
         rad.printData()
 
-    rm.getDarStream(110777)
+    rm.getDarStation(110777)
