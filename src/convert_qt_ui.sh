5939f6c3d3d6ffca7b7d6427901d8992705a0567
Michel Perrocheau <mperrocheau@teclib.com>
2023-03-04 14:31:54 +0100

    [IMP] move script

--- src/convert_qt_ui.sh
+++
@@ -1,6 +0,0 @@
-
-pyuic5 ./qt/mainwindow.ui -x -o ./mainWindow.py
-pyuic5 ./qt/dialogmusicdirectories.ui -x -o ./dialogMusicDirectories.py
-
-
-
---
+++ src/ui/convert_qt_ui.sh
@@ -0,0 +1,6 @@
+
+pyuic5 ../qt/mainwindow.ui -x -o ./mainWindow.py
+pyuic5 ../qt/dialogmusicdirectories.ui -x -o ./dialogMusicDirectories.py
+
+
+
